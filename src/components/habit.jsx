9d2383dcf511c0ae3cd54fc3e1cf647c40438ba8
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-18 22:54:31 +0100

    styling and habit state completed

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count > 0 ? count : 0 });
+  };
   render() {
     return (
       <li className="habit">
         <span className="habitName">Reading</span>
-        <span className="habitCounter">8</span>
-        <button className="habit-btn habit-increase">
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-btn habit-increase"
+          onClick={this.handleIncrement}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-btn habit-decrease">
+        <button
+          className="habit-btn habit-decrease"
+          onClick={this.handleDecrement}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-btn habit-delete">
---
+++ src/components/habits.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Habits extends Component {
+  render() {
+    return <div></div>;
+  }
+}
