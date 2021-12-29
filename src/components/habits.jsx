55f042a1ff5a9422e10fae11720bfaea1cfe5250
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-29 17:41:14 +0100

    navbar and total count func added

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,43 +2,21 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 export default class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: 'Reading', count: 0 },
-      { id: 2, name: 'Running', count: 0 },
-      { id: 3, name: 'Coding', count: 0 },
-    ],
-  };
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    // update
-    habits[index].count++;
-    this.setState({ habits }); //this.setState({ habits : habits});
+    this.props.onIncrement(habit);
   };
 
   handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count > 0 ? count : 0;
-    // if (habits[index].count > 0) {
-    //   habits[index].count--;
-    // }
-    this.setState({ habits });
+    this.props.onDecrement(habit);
   };
 
   handleDelete = (habit) => {
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-
-    // const index = habits.indexOf(habit);
-    // habits.splice(index, 1);
-    this.setState({ habits });
+    this.props.onDelete(habit);
   };
   render() {
     return (
       <ul>
-        {this.state.habits.map((habit) => (
+        {this.props.habits.map((habit) => (
           <Habit
             key={habit.id}
             habit={habit}
