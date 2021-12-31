3bca93d94191c9785a7605fed74c25bf0f191597
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-31 12:21:30 +0100

    fixed habitaddform

--- src/components/habitAddForm.jsx
+++ src/components/habitAddForm.jsx
@@ -1,5 +1,5 @@
 // import React, { Component } from 'react';
-import React from 'react';
+import React, { memo } from 'react';
 
 const HabitAddForm = memo((props) => {
   const formRef = React.createRef();
@@ -9,18 +9,18 @@ const HabitAddForm = memo((props) => {
     const name = inputRef.current.value;
     name && props.onAdd(name);
     formRef.current.reset();
-    return (
-      <form ref={formRef} className="add-form" onSubmit={onSubmit}>
-        <input
-          ref={inputRef}
-          type="text"
-          className="add-input"
-          placeholder="Habit"
-        />
-        <button className="add-button">Add</button>
-      </form>
-    );
   };
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        className="add-input"
+        placeholder="Habit"
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
 });
 
 export default HabitAddForm;
