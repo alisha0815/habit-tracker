dec7f0ace379ce9fc60de5ddebea81f53e89f0d3
Alisha (Jungah Sohn) <77492629+alisha0815@users.noreply.github.com>
2021-12-31 12:56:58 +0100

    simple habit react hook

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,28 +1,49 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class SimpleHabit extends Component {
-  state = {
-    count: 0,
+// react hook (state, lifecycle)
+import React, { useState } from 'react';
+
+const SimpleHabit = (props) => {
+  const [count, setcount] = useState(0);
+  const handleIncrement = () => {
+    setcount(count + 1);
   };
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-
-  render() {
-    return (
-      <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
-        <button
-          className="habit-button habit-increase"
-          onClick={this.handleIncrement}
-        >
-          <i className="fas fa-plus-square"></i>
-        </button>
-      </li>
-    );
-  }
-}
+  return (
+    <li className="habit">
+      <span className="habit-name">Reading</span>
+      <span className="habit-count">{count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
 
 export default SimpleHabit;
+
+// class SimpleHabit extends Component {
+//   state = {
+//     count: 0,
+//   };
+
+//   handleIncrement = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+//   render() {
+//     return (
+//       <li className="habit">
+//         <span className="habit-name">Reading</span>
+//         <span className="habit-count">{this.state.count}</span>
+//         <button
+//           className="habit-button habit-increase"
+//           onClick={this.handleIncrement}
+//         >
+//           <i className="fas fa-plus-square"></i>
+//         </button>
+//       </li>
+//     );
+//   }
+// }
+
+// export default SimpleHabit;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app';
+// import App from './app';
 
 import '@fortawesome/fontawesome-free/js/all.js';
+import SimpleHabit from './components/simpleHabit';
 // import SimpleHabit from './components/simpleHabit';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-    {/* <SimpleHabit /> */}
+    {/* <App /> */}
+    <SimpleHabit />
   </React.StrictMode>,
   document.getElementById('root')
 );
